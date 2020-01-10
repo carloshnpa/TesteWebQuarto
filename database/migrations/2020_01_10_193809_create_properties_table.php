ddd0d2ff43d8e97a8edc77efb086474af7cff182
Carlos Abreu <carlosabreu@192.168.0.124>
2020-01-10 17:09:37 -0300

    create model

---
+++ database/migrations/2020_01_10_193809_create_properties_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->string('photo');
+            $table->text('description');
+            $table->float('value');
+            $table->string('street', 60);
+            $table->string('number', 6);
+            $table->string('postal_code', 8);
+            $table->string('complement', 24)->nullable();
+            $table->string('city', 40);
+            $table->enum('state', ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RR','RO','RJ','RN','RS','SC','SP','SE','TO']);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
