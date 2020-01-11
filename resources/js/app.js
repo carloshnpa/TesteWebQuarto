8feb09e8726172e7f9bb207a9d4f7608eeff8eaa
Carlos Abreu <carlosabreu@192.168.0.124>
2020-01-10 21:43:00 -0300

    Welcome Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import Welcome from './components/Welcome.vue';
+window.Vue = require('vue');
+
+Vue.component('Welcome', Welcome);
+
+const app = new Vue({
+    el: '#app'
+});
